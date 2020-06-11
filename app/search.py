834badfd7d951bb23c678e1a188295f5efa1bcf7
csaba <csaba@upsideenergy.co.uk>
2020-06-11 15:51:56 +0200

    improvement: SearchResults object lives outside context manager

--- app/search.py
+++ app/search.py
@@ -2,13 +2,22 @@ import os
 from whoosh.fields import Schema, TEXT, ID
 from whoosh import index
 from unicodedata import normalize
-from whoosh.qparser import *
+from whoosh.qparser import QueryParser, FuzzyTermPlugin, PhrasePlugin, SequencePlugin
 import whoosh
 from whoosh.scoring import FunctionWeighting
 from whoosh.analysis import CharsetFilter, StemmingAnalyzer
 from whoosh import fields
 from whoosh.support.charset import accent_map
 import os.path
+
+
+class SearchResult:
+
+    def __init__(self, id, score, matched_terms, highlights_text):
+        self.id = id
+        self.score = score
+        self.matched_terms = matched_terms
+        self.highlights_text = highlights_text
 
 
 class Search:
@@ -26,46 +35,54 @@ class Search:
             return matcher.value_as("frequency")
 
         pos_weighting = FunctionWeighting(myscore_fn)
-        
+
         with self.ix.searcher(weighting=pos_weighting) as searcher:
 
             parser = QueryParser("text", self.ix.schema)
-          
+
             if _type == 'fuzzy':
                 parser.add_plugin(FuzzyTermPlugin())
-          
+
             elif _type == 'sequence':
                 parser.remove_plugin_class(PhrasePlugin)
                 parser.add_plugin(SequencePlugin())
                 parser.add_plugin(FuzzyTermPlugin())
-          
+
             query = parser.parse(text)
 
             results = searcher.search(query, terms=True)
-            results.formatter = whoosh.highlight.UppercaseFormatter()
+            results.formatter = whoosh.highlight.HtmlFormatter()
             results.fragmenter = whoosh.highlight.WholeFragmenter()
 
-            return [result.highlights('text') for result in results]
+            search_results = [SearchResult(id=result['id'],
+                                           score=result.score,
+                                           matched_terms=[x[1].decode("utf-8")  for x in result.matched_terms()],
+                                           highlights_text=result.highlights('text'))
+                              for result in results]
+
+            return search_results
 
     def create_index(self, index_dir, text_dir):
 
         # For example, to add an accent-folding filter to a stemming analyzer:
         my_analyzer = StemmingAnalyzer() | CharsetFilter(accent_map)
 
-        schema = Schema(id=ID(stored=True), text=TEXT(stored=True, analyzer=my_analyzer))
+        schema = Schema(id=ID(stored=True), 
+                        text=TEXT(stored=True, analyzer=my_analyzer))
 
         if not os.path.exists(index_dir):
             os.mkdir(index_dir)
 
         ix = index.create_in(index_dir, schema)
-        
+
         writer = ix.writer()
 
-        (_, _, file_names) = next(os.walk(text_dir))       
+        (_, _, file_names) = next(os.walk(text_dir))
 
         for file_name in file_names:
             with open(os.path.join(text_dir, file_name), 'r') as f:
-                writer.add_document(id=f"{file_name.split('.')[0]}", text=normalize('NFKC', f.read()))
+                writer.add_document(
+                    id=f"{file_name.split('.')[0]}", text=normalize('NFKC', f.read()))
 
         writer.commit()
 
